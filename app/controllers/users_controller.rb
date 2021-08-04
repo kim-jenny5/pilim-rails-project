140668073e44e649500f9306f73a91b9b59fec61
Jenny Kim <kimj1905@gmail.com>
2021-08-04 01:58:49 -0400

    Added strong params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,18 @@ class UsersController < ApplicationController
 
     def create
         @user = User.create(user_params)
+        
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            render :new
+        end
+    end
+
+    private
+    
+    def user_params
+        params.require(:user).permit(:first_name, :last_name, :username, :email, :password)
     end
 end
