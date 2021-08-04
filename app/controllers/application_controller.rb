6b74087320b5f184e0bfc2f416094dd3eb612249
Jenny Kim <kimj1905@gmail.com>
2021-08-04 16:35:46 -0400

    added helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
     helper_method :current_user
+    helper_method :logged_in?
+    # before_action :require_login
 
     def home
     end
@@ -8,5 +10,13 @@ class ApplicationController < ActionController::Base
 
     def current_user
         @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
+    end
+
+    def logged_in?
+        !!current_user
+    end
+
+    def require_login
+        redirect_to login_path if !logged_in?
     end
 end
