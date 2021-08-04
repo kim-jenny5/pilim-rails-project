1bd087448c53e03ddc6850c236d71b557750796c
Jenny Kim <kimj1905@gmail.com>
2021-08-04 13:18:06 -0400

    Added validations to signup view

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
     has_many :reviews
     has_many :movies, through: :reviews
 
-    validates :first_name, :last_name, :password, presence: { message: "This field must be provided."}
+    validates :first_name, :last_name, :password, presence: true
     validates :username, presence: true, uniqueness: true
     validates :email, presence: true, uniqueness: true
 end
