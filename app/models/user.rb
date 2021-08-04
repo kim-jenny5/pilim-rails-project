c7b19268c8f30f77036090222d3abdf1abf2cef4
Jenny Kim <kimj1905@gmail.com>
2021-08-04 13:53:30 -0400

    fixed email validation format

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@ class User < ApplicationRecord
     has_many :movies, through: :reviews
 
     validates :first_name, :last_name, :password, presence: true
-    validates :username, presence: true, uniqueness: true
-    validates :email, presence: true, uniqueness: true
+    validates :username, presence: true, uniqueness: { message: "is already taken" }
+    validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/ }, presence: true, uniqueness: { message: "is already taken" }
 end
