5d758b658b2ccaa6ffaa012097f504f23b21a661
Jenny Kim <kimj1905@gmail.com>
2021-08-04 02:23:49 -0400

    adding user validations and fixing sign up form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord    
     has_secure_password
-    
+
     has_many :lists
     has_many :reviews
     has_many :movies, through: :reviews
+
+    validates :first_name, :last_name, :password, presence: { message: "This field must be provided."}
+    validates :username, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true
 end
