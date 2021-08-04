f6c054061114a82b9b439d890a109987786ad3fa
Jenny Kim <kimj1905@gmail.com>
2021-08-04 14:33:50 -0400

    added current user helper method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,6 @@ class UsersController < ApplicationController
 
     def create
         @user = User.create(user_params)
-        byebug
         
         if @user.save
             session[:user_id] = @user.id
