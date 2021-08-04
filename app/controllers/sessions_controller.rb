39dad8c69a4490f32108fd6690c7a5d668cbd3ea
Jenny Kim <kimj1905@gmail.com>
2021-08-04 02:10:49 -0400

    set up session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,16 @@
 class SessionsController < ApplicationController
     def new
-        
+       @user = User.new 
     end
 
     def create
+        if @user = User.find_by_username(params[:username])
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        end
+    end
+
+    def destroy
+        reset_session
     end
 end
