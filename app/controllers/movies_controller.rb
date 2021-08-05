b1ca3c03656b37f906c82563729bfd7bbd70b479
Jenny Kim <kimj1905@gmail.com>
2021-08-05 17:28:03 -0400

    prelim filter by genre done

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,9 +1,12 @@
 class MoviesController < ApplicationController
     def index
-        @newest_movies = Movie.newest_releases
+        # @newest_movies = Movie.newest_releases
         @movie_genres = Movie.alphabetical_genre.select(:genre).distinct
-
-        
+        if params[:genre_name]
+            @movies = Movie.where(genre: params[:genre_name])
+        else
+            @newest_movies = Movie.newest_releases
+        end
     end
 
     def show
