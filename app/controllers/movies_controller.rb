b21d9f67a13727993967f683c4ecc3fc080f988c
Jenny Kim <kimj1905@gmail.com>
2021-08-05 19:50:16 -0400

    adding a second strong params

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,7 +6,7 @@ class MoviesController < ApplicationController
             @movies = Movie.where(genre: params[:genre_name])
         elsif params[:search]
             @movies = Movie.where('title LIKE ?', "%#{params[:search]}%")
-            if @movies.empty?
+            if @movies.blank?
                 @newest_movies = Movie.newest_releases
                 flash[:message] = "'#{params[:search]}' not found."
                 redirect_to movies_path
@@ -24,6 +24,23 @@ class MoviesController < ApplicationController
     end
 
     def create
+        # byebug
+
+        if params[:movie][:new_genre].blank?
+            @test = "yeppers"
+            @movie = Movie.new(movie_params)
+
+            if @movie.save
+                redirect_to movie_path(@movie)
+            else
+                render :new
+            end
+        else
+            byebug
+            @movie = Movie.new(movie_params_new_genre)
+
+        end
+
         @movie = Movie.new(movie_params)
 
         if @movie.save
@@ -41,5 +58,9 @@ class MoviesController < ApplicationController
 
     def movie_params
         params.require(:movie).permit(:title, :genre, :year)
+    end
+
+    def movie_params_new_genre
+        params.require(:movie).permit(:title, :new_genre, :year)
     end
 end
