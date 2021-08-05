3e036fb2b00d70b0454f16d03049211e4c474a3f
Jenny Kim <kimj1905@gmail.com>
2021-08-05 14:31:45 -0400

    trying to figure out scopes for sorting movies by review count

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,9 @@
 class MoviesController < ApplicationController
+    def index
+        # @movies = Movie.all
+        @movies.sort_by_most_reviews
+    end
+
     def show
         @movie = Movie.find_by_id(params[:id])
     end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,4 +3,6 @@ class Movie < ApplicationRecord
     has_many :lists, through: :lists_movies
     has_many :reviews
     has_many :users, through: :reviews
+
+    scope :sort_by_most_reviews, -> { where(reviews: self.maximum(:review))}
 end
