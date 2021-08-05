9650712b2f1bb82cabd4b7c8f256ac70c199d276
Jenny Kim <kimj1905@gmail.com>
2021-08-05 15:39:43 -0400

    fixed movie scope problem

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,10 @@
 class MoviesController < ApplicationController
     def index
         # @movies = Movie.all
-        @movies.sort_by_most_reviews
+        # @movies = Movie.sort_by_most_reviews
+        @movies = Movie.newest_releases
+        # @movies = Movie.joins(:reviews).group("reviews.movie_id").order("count(reviews.movie_id) DESC")
+        # @movies = Movie.joins(:reviews).group("movie.id").order("count(movie.id) DESC")
     end
 
     def show
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,5 +4,9 @@ class Movie < ApplicationRecord
     has_many :reviews
     has_many :users, through: :reviews
 
-    scope :sort_by_most_reviews, -> { where(reviews: self.maximum(:review))}
+    #DOESN'T WORK. WILL NEED TO FIGURE IT OUT
+    # scope :sort_by_most_reviews, -> { where(reviews: self.maximum(:review))}
+    # scope :sort_by_most_reviews, -> { joins(:reviews).group(:title).order(reviews: :desc)}
+    # scope :sort_by_most_reviews, -> { includes(:reviews).order(reviews: :desc)}
+    scope :newest_releases, -> {order(year: :desc)}
 end
