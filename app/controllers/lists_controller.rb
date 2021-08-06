b778dcad25c506b70396fecd43e8ea6b140908ce
Jenny Kim <kimj1905@gmail.com>
2021-08-06 16:39:43 -0400

    added prelim search bar for lists

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,11 +1,25 @@
 class ListsController < ApplicationController
     def index
-        @lists = List.all
-        # @movies = Movie.where('title LIKE ?', "%#{params[:search]}%")
+        if params[:search]
+            @lists = List.where("title LIKE ?", "%#{params[:search]}%")
+            if @lists.blank?
+                flash[:message] = "Sorry. 😅 There's no such list."
+                redirect_to lists_path
+            end
+        else
+            @lists = List.all
+        end
+    end
 
+    def new
+        @list = List.new
     end
 
     def show
         @list = List.find_by_id(params[:id])
     end
+
+    private
+
+    
 end
