752a644a46dfdeb5efedc3e17bbf0851381b49b0
Jenny Kim <kimj1905@gmail.com>
2021-08-05 23:45:03 -0400

    reviews and movie build for review create method done

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,20 +7,23 @@ class ReviewsController < ApplicationController
     def new
         # @user = User.find_by_id(params[:user_id])
         @movie = Movie.find_by_id(params[:movie_id])
-        @review = @movie.reviews.build
+        # @review = @movie.reviews.build
+        @review = Review.new
+        # byebug
     end
 
     def create
-        @review = self.new(review_params)
-
+        @movie = Movie.find_by_id(params[:movie_id])
+        @review = @movie.reviews.build(review_params)
+        # @review = self.new(review_params)
         if @review.save
-            # redirect_to 
+            redirect_to movie_path(@review.movie.id)
         end
     end
 
     private
     
     def review_params
-        params.require(:review).permit(:title, :rating, :comment)
+        params.require(:review).permit(:title, :rating, :comment).merge(user_id: current_user.id)
     end
 end
