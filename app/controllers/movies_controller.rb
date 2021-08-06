95b39bccded8cf1101d20c71c9b5b5715620d5c9
Jenny Kim <kimj1905@gmail.com>
2021-08-06 14:53:29 -0400

    edited star rating for moviews with 0 reviews

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -58,6 +58,6 @@ class MoviesController < ApplicationController
     end
 
     def star_rating(movie)
-        ((movie.reviews.sum(:rating).to_f)/(movie.reviews.count)).round(2)
+        movie.reviews.count == 0 ? "n/a" : ((movie.reviews.sum(:rating).to_f)/(movie.reviews.count)).round(2)
     end
 end
