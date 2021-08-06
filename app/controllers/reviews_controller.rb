dc3118846bfba48484730473fcbc0d0527a815f6
Jenny Kim <kimj1905@gmail.com>
2021-08-05 23:11:15 -0400

    cleaned code and setting up review create method

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,9 +7,20 @@ class ReviewsController < ApplicationController
     def new
         # @user = User.find_by_id(params[:user_id])
         @movie = Movie.find_by_id(params[:movie_id])
-        @review = Review.new 
+        @review = @movie.reviews.build
     end
 
     def create
+        @review = self.new(review_params)
+
+        if @review.save
+            # redirect_to 
+        end
+    end
+
+    private
+    
+    def review_params
+        params.require(:review).permit(:title, :rating, :comment)
     end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,7 +2,7 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :movie
 
-  validates :rating, numericality: { only_integer: true, message: "No decimals please!" }
+  validates :rating, numericality: { only_integer: true }
 
   scope :most_recently_updated, -> { order(updated_at: :desc)}
 end
