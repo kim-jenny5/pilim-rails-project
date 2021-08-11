bbe3393b0600287f4d2b232b58ccbfff6fef5ce9
Jenny Kim <kimj1905@gmail.com>
2021-08-11 17:08:26 -0400

    trying to refactor flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+    # def flash_message(key, value)
+    #     byebug
+    #     flash.each do |key, value|
+    #         content_tag(:div, value, class: "flash #{key}")
+    #     end
+    # end
 end
