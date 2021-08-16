b85bb7dbc6a5dec10dc76fe78aabdd44482b7b08
Jenny Kim <kimj1905@gmail.com>
2021-08-16 13:37:53 -0400

    fixed function to delete list

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,6 @@
 class List < ApplicationRecord
   belongs_to :user
-  has_many :lists_movies
+  has_many :lists_movies, dependent: :destroy
   has_many :movies, through: :lists_movies
 
   validates :title, presence: true
